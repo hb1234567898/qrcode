620a4846a25bf4ae6a97de5ea6ba39b8630f6023
诃荼 <96464799+hb1234567898@users.noreply.github.com>
2023-10-13 22:36:35 +0800

    生成支付二维码

--- index.vue
+++ index.vue
@@ -1,329 +1,309 @@
 <template>
-  <!-- 就诊人组件的静态结构 -->
-  <el-card class="box-card">
-    <template #header>
-      <div class="card-header">
-        <span>就诊人管理</span>
-        <el-button class="button" type="primary" :icon="User" @click="addUser"
-          >添加就诊人</el-button
-        >
+  <div>
+    <!-- 展示订单详情的结构 -->
+    <el-card class="box-card">
+      <!-- 卡片头部的结构 -->
+      <template #header>
+        <div class="detail">挂号详情</div>
+      </template>
+      <!-- 展示身体部分顶部结构 -->
+      <div class="top">
+        <!-- 左侧标签 -->
+        <el-tag class="ml-2" type="success">
+          <div class="tag">
+            <svg
+              t="1685933792381"
+              class="icon"
+              viewBox="0 0 1024 1024"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              p-id="2389"
+              width="16"
+              height="16"
+            >
+              <path
+                d="M392.533333 806.4L85.333333 503.466667l59.733334-59.733334 247.466666 247.466667L866.133333 213.333333l59.733334 59.733334L392.533333 806.4z"
+                fill="#1afa29"
+                p-id="2390"
+              ></path>
+            </svg>
+            <span>{{ orderInfo.param?.orderStatusString }}</span>
+          </div>
+        </el-tag>
+        <div class="right_info">
+          <img src="../../../../assets/images/code_app.png" alt="" />
+          <div class="info">
+            <p>微信 关注 “北京114预约挂号”</p>
+            <p>“快速预约挂号”</p>
+          </div>
+        </div>
       </div>
-    </template>
-    <!-- 就诊人管理模块展示就诊人信息的结构 -->
-    <div class="visitors" v-if="scene == 0">
-      <Visitor
-        @changeScene="changeScene"
-        @removeUser="removeUser"
-        class="item"
-        v-for="(user, index) in userArr"
-        :key="user.id"
-        :user="user"
-        :index="index"
-      />
-    </div>
-    <!-- 添加就诊人|修改已有的就诊人信息的结构 -->
-    <div class="form" v-if="scene == 1">
-      <el-divider content-position="left">就诊人信息</el-divider>
-      <el-form style="width: 60%; margin: 10px auto">
-        <el-form-item label="用户姓名">
-          <el-input placeholder="请你输入用户姓名" v-model="userParams.name"></el-input>
-        </el-form-item>
-        <el-form-item label="证件类型">
-          <el-select
-            placeholder="请你选择证件的类型"
-            style="width: 100%"
-            v-model="userParams.certificatesType"
+      <!-- 订单详情底部的结构 -->
+      <div class="bottom">
+        <div class="left">
+          <!-- 左侧展示订单详情信息 -->
+          <el-descriptions class="margin-top" :column="1" border>
+            <el-descriptions-item>
+              <template #label>
+                <div class="cell-item">就诊人信息</div>
+              </template>
+              {{ orderInfo.patientName }}
+            </el-descriptions-item>
+            <el-descriptions-item>
+              <template #label>
+                <div class="cell-item">就诊日期</div>
+              </template>
+              {{ orderInfo.reserveDate }}
+            </el-descriptions-item>
+            <el-descriptions-item>
+              <template #label>
+                <div class="cell-item">就诊医院</div>
+              </template>
+              {{ orderInfo.hosname }}
+            </el-descriptions-item>
+            <el-descriptions-item>
+              <template #label>
+                <div class="cell-item">就诊科室</div>
+              </template>
+              {{ orderInfo.depname }}
+            </el-descriptions-item>
+            <el-descriptions-item>
+              <template #label>
+                <div class="cell-item">医生职称</div>
+              </template>
+              {{ orderInfo.title }}
+            </el-descriptions-item>
+            <el-descriptions-item>
+              <template #label>
+                <div class="cell-item">医事服务费</div>
+              </template>
+              <span style="color: red">{{ orderInfo.amount }}</span>
+            </el-descriptions-item>
+            <el-descriptions-item>
+              <template #label>
+                <div class="cell-item">挂号单号</div>
+              </template>
+              {{ orderInfo.outTradeNo }}
+            </el-descriptions-item>
+            <el-descriptions-item>
+              <template #label>
+                <div class="cell-item">挂号时间</div>
+              </template>
+              {{ orderInfo.createTime }}
+            </el-descriptions-item>
+          </el-descriptions>
+          <div
+            class="btn"
+            v-if="orderInfo.orderStatus == 0 || orderInfo.orderStatus == 1"
           >
-            <el-option
-              v-for="item in certationArr"
-              :key="item.id"
-              :label="item.name"
-              :value="item.value"
-            ></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="证件号码">
-          <el-input
-            placeholder="请你输入证件号码"
-            v-model="userParams.certificatesNo"
-          ></el-input>
-        </el-form-item>
-        <el-form-item label="用户性别">
-          <el-radio-group v-model="userParams.sex">
-            <el-radio :label="1">男</el-radio>
-            <el-radio :label="0">女</el-radio>
-          </el-radio-group>
-        </el-form-item>
-        <el-form-item label="出生日期">
-          <el-date-picker
-            v-model="userParams.birthdate"
-            value-format="YYYY-MM-DD"
-            type="date"
-            placeholder="请你选择日期"
-          />
-        </el-form-item>
-        <el-form-item label="手机号码">
-          <el-input
-            placeholder="请你输入用户手机号码"
-            v-model="userParams.phone"
-          ></el-input>
-        </el-form-item>
-      </el-form>
-      <el-divider content-position="left"
-        >建档信息（完善后部分医院首次就诊不排队建档）</el-divider
-      >
-      <el-form style="width: 60%; margin: 10px auto">
-        <el-form-item label="婚姻状况">
-          <el-radio-group v-model="userParams.isMarry">
-            <el-radio :label="1">已婚</el-radio>
-            <el-radio :label="0">未婚</el-radio>
-          </el-radio-group>
-        </el-form-item>
-        <el-form-item label="自费/医保">
-          <el-radio-group v-model="userParams.isInsure">
-            <el-radio :label="1">自费</el-radio>
-            <el-radio :label="0">医保</el-radio>
-          </el-radio-group>
-        </el-form-item>
-        <el-form-item label="当前的住址">
-          <el-cascader :props="props" v-model="userParams.addressSelected" />
-        </el-form-item>
-        <el-form-item label="详细地址">
-          <el-input
-            placeholder="请你输入用户详细地址"
-            v-model="userParams.address"
-          ></el-input>
-        </el-form-item>
-      </el-form>
-      <el-divider content-position="left">联系人信息（选填）</el-divider>
-      <el-form style="width: 60%; margin: 10px auto" label-width="80">
-        <el-form-item label="用户姓名">
-          <el-input
-            placeholder="请你输入用户姓名"
-            v-model="userParams.contactsName"
-          ></el-input>
-        </el-form-item>
-        <el-form-item label="证件类型">
-          <el-select
-            placeholder="请你选择证件的类型"
-            style="width: 100%"
-            v-model="userParams.contactsCertificatesType"
-          >
-            <el-option
-              v-for="item in certationArr"
-              :key="item.id"
-              :label="item.name"
-              :value="item.value"
-            ></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="证件号码">
-          <el-input
-            placeholder="请你输入证件号码"
-            v-model="userParams.contactsCertificatesNo"
-          ></el-input>
-        </el-form-item>
-        <el-form-item label="手机号码">
-          <el-input
-            placeholder="请你输入用户手机号码"
-            v-model="userParams.contactsPhone"
-          ></el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" size="default" @click="submit">提交</el-button>
-          <el-button type="primary" size="default" @click="reset">重写</el-button>
-        </el-form-item>
-      </el-form>
-    </div>
-  </el-card>
+            <el-popconfirm title="确定取消预约吗?" @confirm="cancel">
+              <template #reference>
+                <el-button>取消预约</el-button>
+              </template>
+            </el-popconfirm>
+            <el-button
+              type="primary"
+              size="default"
+              v-if="orderInfo.orderStatus == 0"
+              @click="openDialog"
+              >支付</el-button
+            >
+          </div>
+        </div>
+        <div class="right">
+          <el-card class="box-card">
+            <template #header>
+              <div class="card-header">
+                <span>注意事项</span>
+              </div>
+            </template>
+            <p>1.请确认就诊人信息是否准确，若填写错误将无法取号就诊，损失由本人承担；</p>
+            <p style="color: red">
+              2.【取号】就诊当天需在{{ orderInfo.fetchTime }}前
+              在医院取号，未取号视为爽约，该号不退不换；
+            </p>
+            <p>
+              3.【退号】在{{ orderInfo.quitTime }}前可在线退号 ，逾期将不可办理退号退费；
+            </p>
+            <p>
+              4.北京114预约挂号支持自费患者使用身份证预约，同时支持北京市医保患者使用北京社保卡在平台预约挂号。请于就诊当日，携带预约挂号所使用的有效身份证件到院取号；
+            </p>
+            <p>5.请注意北京市医保患者在住院期间不能使用社保卡在门诊取号。</p>
+          </el-card>
+        </div>
+      </div>
+    </el-card>
+    <!-- 展示支付二维码的结构 -->
+    <!-- 对话框通过v-model控制显示与隐藏的 true:展示 false隐藏 -->
+    <el-dialog @close="close" v-model="dialogVisible" title="微信支付" width="400">
+      <!-- 支付需要使用的二维码图片 -->
+      <div class="qrocde">
+        <img :src="imgUrl" alt="" />
+        <p>请使用微信扫一扫</p>
+        <p>扫描二维码支付</p>
+      </div>
+      <!-- 对话框底部插槽传递结构 -->
+      <template #footer>
+        <el-button type="primary" size="default" @click="closeDialog">关闭窗口</el-button>
+      </template>
+    </el-dialog>
+  </div>
 </template>
 
 <script setup lang="ts">
-//@ts-ignore
-import type { CascaderProps } from "element-plus";
-import { User } from "@element-plus/icons-vue";
-import { reqGetUser } from "@/api/hospital";
-import { reqCertationType, reqCity, reqAddOrUpdateUser } from "@/api/user";
-import { onMounted, ref, reactive, watch } from "vue";
-import type { UserResponseData, UserArr } from "@/api/hospital/type";
-import type {
-  CertationTypeResponseData,
-  CertationArr,
-  AddOrUpdateUser,
-} from "@/api/user/type";
-//引入路由器与路由方法
-import { useRouter, useRoute } from "vue-router";
-let $route = useRoute();
-let $router = useRouter();
+import { onMounted, ref } from "vue";
+import { reqOrderInfo, reqCancelOrder, reqQrcode, reqQueryPayState } from "@/api/user";
+import { useRoute } from "vue-router";
+import type { PayReslt, OrderResponseData, QrCode } from "@/api/user/type";
 //@ts-ignore
 import { ElMessage } from "element-plus";
-//存储全部就诊人的信息
-let userArr = ref<UserArr>([]);
-//定义一个响应式数据:决定卡片的身体部分的展示内容
-let scene = ref<number>(0);
-//存储证件类型的数据
-let certationArr = ref<CertationArr>([]);
-//收集新增就诊人的数据
-let userParams = reactive<AddOrUpdateUser>({
-  name: "",
-  certificatesType: "",
-  certificatesNo: "",
-  sex: 0,
-  birthdate: "",
-  phone: "",
-  isMarry: 0,
-  isInsure: 0,
-  addressSelected: [],
-  address: "",
-  contactsName: "",
-  contactsCertificatesType: "",
-  contactsCertificatesNo: "",
-  contactsPhone: "",
-});
-//组件挂载完毕获取一次就诊人的信息
+//生成二维码插件qrcode
+//@ts-ignore
+import QRCode from "qrcode";
+let $route = useRoute();
+//定义存储二维码图片路径
+let imgUrl = ref<string>("");
+//控制对话框显示与隐藏的数据
+let dialogVisible = ref<boolean>(false);
+let orderInfo = ref<any>({});
+//存储定时器引用
+let timer = ref<any>();
+//组件挂载完毕
 onMounted(() => {
-  //获取就诊人信息
-  getAllUser();
-  //获取证件的类型的数据
-  getCertationType();
-  //判断:当前这个路由组件是不是从挂号组件而来[挂号组件而来,路径上是携带query参数type=add]
-  if ($route.query.type == "add") {
-    scene.value = 1;
-  }
-
-  if ($route.query.type == "edit") {
-    scene.value = 1;
-  }
+  getOrderInfo();
 });
-//获取全部就诊人信息
-const getAllUser = async () => {
-  let result: UserResponseData = await reqGetUser();
+//获取订单详情的数据
+const getOrderInfo = async () => {
+  let result: OrderResponseData = await reqOrderInfo($route.query.orderId as string);
   if (result.code == 200) {
-    userArr.value = result.data;
-  }
-};
-//添加就诊人按钮的回调
-const addUser = () => {
-  //清空上一次的数据
-  reset();
-  //切换场景为1
-  scene.value = 1;
-};
-//就诊人子组件自定义事件的回调
-const changeScene = (user: AddOrUpdateUser) => {
-  scene.value = 1;
-  //收集已有的就诊人信息
-  Object.assign(userParams, user);
-};
-
-const reset = () => {
-  Object.assign(userParams, {
-    id:null,
-    name: "",
-    certificatesType: "",
-    certificatesNo: "",
-    sex: 0,
-    birthdate: "",
-    phone: "",
-    isMarry: 0,
-    isInsure: 0,
-    addressSelected: [],
-    address: "",
-    contactsName: "",
-    contactsCertificatesType: "",
-    contactsCertificatesNo: "",
-    contactsPhone: "",
-  });
-};
-//获取证件类型的接口
-const getCertationType = async () => {
-  let result: CertationTypeResponseData = await reqCertationType();
-  if (result.code == 200) {
-    certationArr.value = result.data;
+    orderInfo.value = result.data;
   }
 };
 
-//级联选择器数据
-const props: CascaderProps = {
-  lazy: true, //懒加载数据
-  //加载级联选择器数据方法
-  async lazyLoad(node: any, resolve: any) {
-    let result: any = await reqCity(node.data.id || "86");
-    //整理数据
-    let showData = result.data.map((item: any) => {
-      return {
-        id: item.id,
-        label: item.name,
-        value: item.value,
-        leaf: !item.hasChildren,
-      };
-    });
-    //注入组件需要展示的数据
-    resolve(showData);
-  },
-};
-
-//提交按钮的方法的回调
-const submit = async () => {
-  //要么新增就诊人|更新一个已有的就诊人
+//取消订单   订单状态有三种 orderStatus  -1  取消预约  0 预约但是没有支付  1 支付成功
+const cancel = async () => {
   try {
-    //要么新增用户成功|更新已有的用户成功
-    await reqAddOrUpdateUser(userParams);
-    //提示文字
-    ElMessage({
-      type: "success",
-      message: userParams.id ? "更新成功" : "新增成功",
-    });
-    //提交按钮的时候判断是不是从预约挂号而来
-    if ($route.query.type) {
-      $router.back();
-    } else {
-      scene.value = 0;
-    }
-    //在获取全部的就诊人的信息
-    getAllUser();
+    //取消预约成功
+    await reqCancelOrder($route.query.orderId as string);
+    //再次获取订单详情的数据
+    getOrderInfo();
   } catch (error) {
     ElMessage({
-      type: "success",
-      message: userParams.id ? "更新失败" : "新增失败",
+      type: "error",
+      message: "取消预约失败",
     });
   }
 };
 
-//监听全部就诊人的数据
-watch(
-  () => userArr.value,
-  () => {
-    if ($route.query.type == "edit") {
-       let user  = userArr.value.find((item:any)=>{
-           return item.id==$route.query.id;
-       });
-      Object.assign(userParams,user)
-    }
-  }
-);
+//点击支付按钮的回调
+const openDialog = async () => {
+  //展示对话框
+  dialogVisible.value = true;
+  //获取支付需要使用二维码信息
+  let result: QrCode = await reqQrcode($route.query.orderId as string);
+  //更具服务器返回二维码信息生成二维码图片
+  imgUrl.value = await QRCode.toDataURL(result.data.codeUrl);
+  //询问服务器当前这笔交易的支付结果
+  //只要二维码查来:需要每隔几秒询问服务器是否支付成功
 
-//子组件自定义事件:删除按钮触发
-const removeUser = ()=>{
-  //再次获取全部的就诊人的信息
-   getAllUser();
+  timer.value = setInterval(async () => {
+    let result: PayReslt = await reqQueryPayState($route.query.orderId as string);
+    //如果服务器返回的数据data:true,代表支付成功
+    if (result.data) {
+      //关闭对话框
+      dialogVisible.value = false;
+      //提示信息
+      ElMessage({
+        type: "success",
+        message: "支付成功",
+      });
+      //清除定时器
+      clearInterval(timer.value);
+      //再次获取订单详情的数据
+      getOrderInfo();
+    }
+  }, 2000);
+};
+//关闭窗口的回调
+const closeDialog = () => {
+  //关闭对话框,对话框隐藏
+  dialogVisible.value = false;
+  //清除定时器
+  clearInterval(timer.value);
+};
+//对话框右上角关闭的叉子的回调
+const close = ()=>{
+  clearInterval(timer.value);
 }
 </script>
 
 <style scoped lang="scss">
-.card-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+.box-card {
+  .detail {
+    color: #7f7f7f;
+    font-weight: 900;
+  }
+  .top {
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 1px solid #ccc;
+    padding: 10px;
+    .tag {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      span {
+        margin-left: 5px;
+      }
+    }
+    .right_info {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      img {
+        width: 40px;
+        height: 40px;
+      }
+      .info {
+        margin-left: 10px;
+        p {
+          font-size: 12px;
+          line-height: 20px;
+        }
+      }
+    }
+  }
+
+  .bottom {
+    display: flex;
+    margin-top: 20px;
+    .left {
+      flex: 4;
+      .btn {
+        margin: 10px 0px;
+      }
+    }
+    .right {
+      margin-left: 20px;
+      flex: 6;
+      p {
+        line-height: 30px;
+      }
+    }
+  }
 }
 
-.visitors {
-  display: flex;
-  flex-wrap: wrap;
+::v-deep(.el-dialog__body) {
+  border-top: 1px solid #7f7f7f;
+  border-bottom: 1px solid #7f7f7f;
+}
 
-  .item {
-    width: 32%;
-    margin: 5px;
+.qrocde {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  p {
+    line-height: 30px;
   }
 }
 </style>
